15782d62188bdecc3336484b1e15b18354e8536d
Miah Clay <miahb88@gmail.com>
2022-06-02 10:10:01 -0400

    working

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,7 +1,21 @@
 function caesarCipher(str, num) {
-    
+    let abcLow = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    let abcUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY"
+
+    for (let i=0; i<str.length; i++) {
+        
+    }
+
+
+
 }
+
+console.log(caesarCipher("Boy! What a string!", -5) === "Wjt! Rcvo v nomdib!")
+console.log(caesarCipher("Hello zach168! Yes here.", 5) === "Mjqqt efhm168! Djx mjwj.")
+console.log(caesarCipher("Hello Zach168! Yes here.", -5) === "Czggj Uvxc168! Tzn czmz.")
+
+
 
 // exports.caesarCipher = function() {
 
-};
+// };
